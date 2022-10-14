85d2259b6cedf403ca101e7743e6a8767e92bf5d
Gosia Janińska <malgorzata.janinska@gmail.com>
2022-10-14 04:13:04 +0200

    add Main component to pages

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,4 @@
+import { Main } from "../components/Main"
 import { Footer } from "../components/Footer"
 import { Header } from "../components/Header"
 
@@ -5,11 +6,7 @@ const About = () => {
   return (
     <div className="container md:px-4 flex flex-col bg-green-300 min-h-screen">
       <Header></Header>
-      <main className="bg-green-500 flex-grow">
-        <div className="p-4">
-          <h1>About</h1>
-        </div>
-      </main>
+      <Main><h1>About</h1></Main>
       <Footer></Footer>
     </div>
   )
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,15 +1,16 @@
-import { Footer } from "../../components/Footer"
 import { Header } from "../../components/Header"
+import { Main } from "../../components/Main"
+import { Footer } from "../../components/Footer"
 
 const Users = () => {
   return (
     <div className="container md:px-4 flex flex-col bg-green-300 min-h-screen">
       <Header></Header>
-      <main className="bg-yellow-200 flex-grow">
-        <div className="p-4">
-          <h1>Users</h1>
-        </div>
-      </main>
+
+      <Main>
+        <h1>Users</h1>
+      </Main>
+
       <Footer></Footer>
     </div>
   )
