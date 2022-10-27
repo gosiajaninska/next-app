c9ec9dca3e5c79c83dee7534f23d4d5050e96801
Gosia Janińska <malgorzata.janinska@gmail.com>
2022-10-27 16:53:29 +0200

    product styling

--- components/Product.tsx
+++ components/Product.tsx
@@ -12,15 +12,17 @@ interface ProductProps {
 export const Product = ({ name, imgUrl, imgAlt, price, id }: ProductProps) => {
   return (
     <Link href={`products/${id}`}>
-      <a className="block">
-        <Image
-          src={imgUrl}
-          alt={imgAlt}
-          width={650}
-          height={650}
-          className="aspect-square w-full object-cover"
-        />
-        <div className="mt-2">
+      <a className="block border border-gray-100 hover:border-gray-300 overflow-hidden shadow-lg">
+        <div className="bg-white p-8 shadow-gray-200 shadow-md">
+          <Image
+            src={imgUrl}
+            alt={imgAlt}
+            width={650}
+            height={650}
+            className="aspect-square w-full object-contain"
+          />
+        </div>
+        <div className="p-4 bg-white h-full">
           <h3 className="font-medium">{name}</h3>
           <p className="mt-1 text-sm text-slate-700">${price}</p>
         </div>
