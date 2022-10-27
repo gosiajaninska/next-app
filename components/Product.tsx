f6af13008e9144e96c9d719e61fb3667a58a9ce7
Gosia Janińska <malgorzata.janinska@gmail.com>
2022-10-28 00:02:44 +0200

    product component details + general product type

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,30 +1,40 @@
-import Link from "next/link"
-import Image from 'next/image'
+import Link from "next/link";
+import Image from 'next/image';
 
-interface ProductProps {
-  id: number,
-  name: string,
-  imgUrl: string,
-  imgAlt: string,
-  price: number,
+interface Product {
+  id:          number;
+  name:        string;
+  price:       number;
+  desc:        string;
+  category:    string;
+  imgUrl:      string;
+  imgAlt:      string;
+  rating: {
+    rate:      number;
+    count:     number;
+  };
 }
 
-export const Product = ({ name, imgUrl, imgAlt, price, id }: ProductProps) => {
+interface ProductProps {
+  productData: Product;
+}
+
+export const Product = ({ productData }: ProductProps) => {
   return (
-    <Link href={`products/${id}`}>
+    <Link href={`products/${productData.id}`}>
       <a className="block border border-gray-100 hover:border-gray-300 overflow-hidden shadow-lg">
         <div className="bg-white p-8 shadow-gray-200 shadow-md">
           <Image
-            src={imgUrl}
-            alt={imgAlt}
+            src={productData.imgUrl}
+            alt={productData.imgAlt}
             width={650}
             height={650}
             className="aspect-square w-full object-contain"
           />
         </div>
         <div className="p-4 bg-white h-full">
-          <h3 className="font-medium">{name}</h3>
-          <p className="mt-1 text-sm text-slate-700">${price}</p>
+          <h3 className="font-medium">{productData.name}</h3>
+          <p className="mt-1 text-sm text-slate-700">${productData.price}</p>
         </div>
       </a>
     </Link>
