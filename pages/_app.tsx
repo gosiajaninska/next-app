85f050598a5c8cad55976cb080e1a276c758e52e
Gosia Janińska <malgorzata.janinska@gmail.com>
2022-10-29 12:25:11 +0200

    sale page ssg -> csg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,21 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Header } from "../components/Header"
 import { Footer } from "../components/Footer"
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const queryClient = new QueryClient()
+
   return (
-    <div className="container flex flex-col min-h-screen relative">
-      <Header></Header>
-      <Component {...pageProps} />
-      <Footer></Footer>
-    </div>
-    )
+    <QueryClientProvider client={queryClient}>
+      <div className="container flex flex-col min-h-screen relative">
+        <Header></Header>
+        <Component {...pageProps} />
+        <Footer></Footer>
+      </div>
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
--- pages/sale.tsx
+++ pages/sale.tsx
@@ -1,13 +1,30 @@
-import { InferGetStaticPropsType } from "next";
 import { Main } from "../components/Main";
 import { ProductListItem } from "../components/Product";
+import { useQuery } from '@tanstack/react-query';
 
-const SalePage = ({ products }: InferGetStaticPropsType<typeof getStaticProps>) => {
+const getProducts = async () => {
+  const response = await fetch(`https://fakestoreapi.com/products/`);
+  const products: StoreApiResponse[] = await response.json();
+  return products;
+}
+
+const SalePage = () => {
+
+  const { data, isLoading, isError } = useQuery(["products"], getProducts);
+
+  if (isLoading) { 
+    return <Main><div>Loading...</div></Main>;
+  }
+
+  if (!data || isError) {
+    return <Main><div>ups...</div></Main>;
+  }
+
   return (
     <Main cssClass="flex flex-col justify-center">
       <div className="p-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-16 lg:grid-cols-4">
         {
-          products.map(product => {
+          data.map(product => {
             return <ProductListItem 
               key={product.id}
               productData={{
@@ -39,18 +56,3 @@ export interface StoreApiResponse {
     count:     number;
   };
 }
-
-export const getStaticProps = async () => {
-  const response = await fetch('https://fakestoreapi.com/products/');
-  const products: StoreApiResponse[] = await response.json();
-
-  return {
-    props: {
-      products,
-    }
-  };
-};
-
-
-
-
