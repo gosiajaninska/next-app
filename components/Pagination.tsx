b9b3eac9e56bc8ca809da32ba2c910f0dc99f65e
Gosia Janińska <malgorzata.janinska@gmail.com>
2022-10-31 17:13:57 +0100

    add static pagination component

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 
 interface PaginationProps {
   activePageNumber: number,
@@ -14,30 +16,59 @@ export const Pagination = ({ activePageNumber, pagesQuantity, onClick }: Paginat
 
   const pageLink = (pageNumber:number, isActive:boolean) => {
     return (
-      <a
-        key={pageNumber}
-        href="#"
-        onClick={clickHandler}
-        data-page={pageNumber}
-        className={`border-transparent ${isActive ? "border-t-gray-500" : "border-t-transparent"} hover:bg-gray-200 border-t-4 border-b-4 py-4 px-4 inline-flex items-center`}
-      >
-        {pageNumber}
-      </a>    
+      <Link href="#" key={pageNumber}>
+        <a
+          onClick={clickHandler}
+          data-page={pageNumber}
+          className={`border-transparent ${isActive ? "border-t-gray-500" : "border-t-transparent"} hover:bg-gray-200 border-t-4 border-b-4 py-4 px-4 inline-flex items-center`}
+        >
+          {pageNumber}
+        </a>   
+      </Link> 
     )
   }
 
-  const allPagesLinks = (activePageNumber:number, pagesQuantity:number) => {
-    let links = [];
-    for (let i = 1; i <= pagesQuantity; i++) {
-      links.push( pageLink(i, activePageNumber == i) );
-    }
-    return links;
+  return renderNav(activePageNumber, pagesQuantity, pageLink);
+}
+
+
+
+
+interface PaginationStaticProps {
+  activePageNumber: number,
+  pagesQuantity: number,
+}
+
+export const PaginationStatic = ({ activePageNumber, pagesQuantity }: PaginationStaticProps) => {
+
+  const pageLink = (pageNumber:number, isActive:boolean) => {
+    return (
+      <Link href={pageNumber.toString()} key={pageNumber}>
+        <a
+          className={`border-transparent ${isActive ? "border-t-gray-500" : "border-t-transparent"} hover:bg-gray-200 border-t-4 border-b-4 py-4 px-4 inline-flex items-center`}
+        >
+          {pageNumber}
+        </a>    
+      </Link>
+    )
   }
 
+  return renderNav(activePageNumber, pagesQuantity, pageLink);
+}
 
+
+
+
+const renderNav = (activePageNumber:number, pagesQuantity:number, pageLink:Function) => {
+  let links = [];
+  for (let i = 1; i <= pagesQuantity; i++) {
+    links.push( pageLink(i, activePageNumber == i) );
+  }
+  
   return (
     <nav className="border-t border-gray-500 px-4 flex items-center justify-center sm:px-0">
-      { allPagesLinks(activePageNumber, pagesQuantity) }
+      { links }
     </nav>
-  )
+  );
 }
+
