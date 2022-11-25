368672f9dd2e11b7a6f4e173d23ef528d1b77ba2
Gosia Janińska <malgorzata.janinska@gmail.com>
2022-11-25 23:35:59 +0100

    render external links without <Link> component

--- components/MyMarkdown.tsx
+++ components/MyMarkdown.tsx
@@ -9,16 +9,46 @@ export const MyMarkdown = ({ children }: { children: MarkdownResult }) => {
       {...children} 
       components={{
         a: ({ href, ...props }) => {
+
           if (!href) {
             return <a { ...props }></a>;
           }
-          return (
-            <Link href={ href }>
-              <a { ...props }></a>
-            </Link>
-          )
+
+          if (linkIsExternal(href)) {
+            return <a href={ href } { ...props } rel="noopener noreferrer" className="bg-red-700 text-white px-2 py-1 inline-block m-1"></a>;
+          } else {
+            return <Link href={ href }><a { ...props } className="bg-blue-700 text-white px-2 py-1 inline-block m-1"></a></Link>;
+          }
+
         }
       }}
     />
   )
 }
+
+
+const linkIsExternal = (href: string) => {
+  let isExternal = false;
+  const [hrefProtocol, hrefRest] = href.split("://");
+  const hrefHost = hrefRest?.split("/")[0];
+
+  const hasProtocol = hrefProtocol === "http" || hrefProtocol === "https";
+  const hostIsEqual = hrefHost === appHost();
+
+  if (hasProtocol && !hostIsEqual) {
+    isExternal = true;
+  }
+
+  return isExternal;
+}
+
+const appHost = () => {
+  const url = process.env.NEXT_PUBLIC_HOST;
+  
+  if (!url) {
+    throw new Error(`Missing NEXT_PUBLIC_HOST env variable!`);
+  }
+
+  return url;
+}
+
