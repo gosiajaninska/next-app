cc19d7b0016221937e5812719afb2d7a7f9b847a
Gosia Janińska <malgorzata.janinska@gmail.com>
2022-11-27 14:25:14 +0100

    add cart page

---
+++ pages/cart.tsx
@@ -0,0 +1,9 @@
+import { Main } from "../components/Main";
+
+const CartPage = () => {
+  return (
+    <Main><h1 className="uppercase text-lg font-bold text-slate-600 py-12 px-8">Cart</h1></Main>
+  )
+}
+
+export default CartPage;
