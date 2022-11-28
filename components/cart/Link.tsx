a874eff8c7fdda64f132ee7dc9d82390b5910058
Gosia Janińska <malgorzata.janinska@gmail.com>
2022-11-28 11:56:59 +0100

    display cart context

--- components/cart/Link.tsx
+++ components/cart/Link.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { useContext } from "react";
+import { CartStateContext } from "./Context";
 import { CartIcon } from "./Icon";
 
 interface CartLinkProps {
@@ -6,10 +8,17 @@ interface CartLinkProps {
 }
 
 export const CartLink = ({ isActive }: CartLinkProps) => {
+  const CartState = useContext(CartStateContext);
+
   return <Link href="/cart">
     <a
-      className={`flex items-center h-full text-gray-500 border-b-4 border-${isActive ? 'current' : 'transparent'} p-6 hover:opacity-75`}
+      className={`flex items-center gap-2 h-full text-gray-500 border-b-4 border-${isActive ? 'current' : 'transparent'} p-6 hover:opacity-75`}
     >
+      <span 
+        className="bg-red-400 text-white font-bold text-xs rounded-full aspect-square w-6 h-6 flex items-center justify-center"
+      >
+        {CartState?.items.length}
+      </span>
       <CartIcon></CartIcon>
       <span className="sr-only">Cart</span>
     </a>
--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,8 +1,26 @@
+import { useContext } from "react";
+import { CartStateContext } from "../components/cart/Context";
 import { Main } from "../components/Main";
 
 const CartPage = () => {
+  const CartState = useContext(CartStateContext);
+
   return (
-    <Main><h1 className="uppercase text-lg font-bold text-slate-600 py-12 px-8">Cart</h1></Main>
+    <Main>
+      <h1 className="uppercase text-lg font-bold text-slate-600 py-12 px-8">Cart</h1>
+      <ul className="px-8">
+        { 
+          CartState?.items.map((item, i) => {
+            return (
+              <li key={i} className="flex justify-between border-b border-gray-300 py-4 px-8">
+                <span>{item.title}</span> 
+                <span>{item.price}</span>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </Main>
   )
 }
 
