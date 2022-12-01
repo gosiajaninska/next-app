4e86a41f7f60c09f8028c67bc7ca45e8716654a5
Gosia Janińska <malgorzata.janinska@gmail.com>
2022-12-01 01:38:33 +0100

    cart layout changes

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,6 +1,7 @@
 import { Main } from "../components/Main";
 import { useCartState } from "../components/cart/Context";
 import { DecreaseCartItemButton, IncreaseCartItemButton } from "../components/cart/Button";
+import React from "react";
 
 const CartPage = () => {
   const cartState = useCartState();
@@ -8,25 +9,30 @@ const CartPage = () => {
   return (
     <Main>
       <h1 className="uppercase text-lg font-bold text-slate-600 py-12 px-8">Cart</h1>
-      <div className="grid md:grid-cols-2">
-        <ul className="px-8 pb-12 divide-y divide-slate-300">
+      <div className="grid md:grid-cols-3 gap-12 px-8">
+        <div className="col-span-2 grid grid-cols-12 gap-y-4 text-slate-700">
           { 
-            cartState.items.map((item, index) => {
+            cartState.items.map((item) => {
               return (
-                <li key={item.id} className="flex justify-between py-4 px-8 text-slate-700">
-                  <span>{item.amount} &times; {item.title}</span> 
-                  <span>{item.price}</span>
-                  <IncreaseCartItemButton id={item.id} title={item.title} price={item.price} />
-                  <DecreaseCartItemButton id={item.id} />
-                </li>
+                <React.Fragment key={item.id}>
+                  <div className="col-span-1"><IncreaseCartItemButton id={item.id} title={item.title} price={item.price} /></div>
+                  <div className="col-span-1">{item.amount}</div>
+                  <div className="col-span-1"><DecreaseCartItemButton id={item.id} /></div>                  
+                  <div className="col-span-6">{item.title}</div> 
+                  <div className="col-span-3 text-right">{item.price * item.amount}</div>
+                </React.Fragment>
               )
             })
           }
-        </ul>
-        <div className="px-8">
+        </div>
+        <div>
           <p>
             Items in cart: 
             <b> { cartState.items.map(item => item.amount).reduce((total, num) => total + num, 0) }</b>
+          </p>
+          <p>
+            Total: 
+            <b> { cartState.items.map(item => item.price * item.amount).reduce((total, num) => total + num, 0) }</b>
           </p>
         </div>
       </div>
