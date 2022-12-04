d551d63e3b8a1780a6e849728078b2b65923657f
Gosia Janińska <malgorzata.janinska@gmail.com>
2022-12-04 20:03:11 +0100

    add ProductsListWithPagination component

--- components/ProductsList.tsx
+++ components/ProductsList.tsx
@@ -1,4 +1,5 @@
 import { ProductsListResponse } from "../utility";
+import { Pagination } from "./Pagination";
 import { ProductListItem } from "./Product";
 
 export const ProductsList = ({ products }: ProductsListResponse) => {
@@ -19,5 +20,48 @@ export const ProductsList = ({ products }: ProductsListResponse) => {
         })
       }
     </div>
+  )
+}
+
+
+interface ProductsListWithPaginationProps {
+  products: ProductsListResponse['products'],
+  pageNumber: number,
+  productsPerPage: number,
+  pageChangeFunction: (pageNumber: number) => void,
+}
+
+export const ProductsListWithPagination = ({ products, pageNumber, productsPerPage, pageChangeFunction }: ProductsListWithPaginationProps ) => {
+
+  const productsQuantity = products.length;
+  const pagesQuantity = Math.ceil(productsQuantity / productsPerPage);
+
+  return(
+    <>
+      <p className="mx-16 mt-16 text-xl font-bold text-gray-500">
+        {productsQuantity} products
+      </p>
+      <div className="p-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-16 lg:grid-cols-4">
+        {
+          products.map(product => {
+            return <ProductListItem 
+              key={product.id}
+              productData={{
+                id:     product.id,
+                name:   product.name,
+                imgUrl: product.images[0].url,
+                imgAlt: product.name,
+                price:  product.price,
+              }}
+            />;
+          })
+        }
+      </div>
+      <Pagination 
+        pagesQuantity={pagesQuantity}
+        activePageNumber={pageNumber}
+        onClick={pageChangeFunction}
+      />
+    </>
   )
 }
