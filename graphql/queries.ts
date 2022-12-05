80166e1109faf2705033f93df536833f00e48fe2
Gosia Janińska <malgorzata.janinska@gmail.com>
2022-12-06 00:17:33 +0100

    add new queries and functions

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -1,6 +1,8 @@
 import { gql } from "@apollo/client";
+import { ProductDataResponse, ProductsListResponse } from "../utility";
+import { apolloClient } from "./apolloClient";
 
-export const getProductsList = gql`
+export const getProductsListQuery = gql`
   query GetProductsList($skip: Int, $first: Int) {
     products(skip: $skip, first: $first) {
       id
@@ -17,7 +19,7 @@ export const getProductsList = gql`
   `;
 
 
-export const getProductsSlugs = gql`
+export const getProductsSlugsQuery = gql`
   query GetProductsSlugs {
     products {
       slug
@@ -26,7 +28,7 @@ export const getProductsSlugs = gql`
   `; 
 
 
-export const getProductBySlug = gql`
+export const getProductBySlugQuery = gql`
   query GetProductBySlugs($slug: String) {
     product(where: {slug: $slug}) {
       id
@@ -42,3 +44,47 @@ export const getProductBySlug = gql`
     }
   }
 `;
+
+
+export const countProducts = async () => {
+  const { data } = await apolloClient
+    .query<{ products: { slug: string }[] }>({
+      query: getProductsSlugsQuery
+    });
+  return data.products.length;
+}
+
+
+export const getProducts = async (skip: number, first: number) => {
+  const { data } = await apolloClient
+    .query<ProductsListResponse>({
+      query: getProductsListQuery,
+      variables: { skip: skip, first: first}
+    });
+  return data.products;
+}
+
+
+export const countPages = async (productsPerPage=4) => {
+  const productsQuantity = await countProducts();
+  return Math.ceil(productsQuantity / productsPerPage);
+}
+
+
+export const getProductBySlug = async (slug: string) => {
+  const { data } = await apolloClient
+    .query<{ product: ProductDataResponse }>({
+      query: getProductBySlugQuery, 
+      variables: { slug: slug }
+    });
+  return data.product;
+}
+
+
+export const getProductsSlugs = async () => {
+  const { data } = await apolloClient
+    .query<{ products: { slug: string }[] }>({
+      query: getProductsSlugsQuery,
+    });
+  return data.products.map( item => item.slug );
+}
