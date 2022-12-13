9475d26ba746e8cb98e024d8d1b03870e458ff46
Gosia Janińska <malgorzata.janinska@gmail.com>
2022-12-13 16:58:45 +0100

    ref: checkout page

--- components/CheckoutForm.tsx
+++ components/CheckoutForm.tsx
@@ -27,140 +27,127 @@ export const CheckoutForm = () => {
   const onSubmit: SubmitHandler<CheckoutFormData> = data => console.log(data);
 
   return (
-    <section>
-
-      <div className="grid grid-cols-1 mx-auto max-w-screen-2xl md:grid-cols-2 border md:divide-x">
-        <div className="py-16 px-8">
-          <h2 className="uppercase text-md font-bold text-slate-600 mb-12 ">Cart summary</h2>
-        </div>
-
-        <div className="py-16 px-8">
-          <div className="max-w-lg px-8 mx-auto">
-            <form 
-              onSubmit={handleSubmit(onSubmit)} 
-              className="grid grid-cols-6 gap-4"
-            >
-              <div className="col-span-6">
-                <Input 
-                  id="FirstName"
-                  label="Name"
-                  autocomplete="name"
-                  inputAttributes={{...register("name")}}
-                  errorMessage={errors.name?.message}
-                />
-              </div>
-
-              <div className="col-span-6">
-                <Input 
-                  id="Email"
-                  label="E-mail"
-                  autocomplete="email"
-                  type="email"
-                  inputAttributes={{...register("email")}}
-                  errorMessage={errors.email?.message}
-                />
-              </div>
-
-              <div className="col-span-6">
-                <Input 
-                  id="phone"
-                  label="Phone"
-                  autocomplete="tel"
-                  type="tel"
-                  inputAttributes={{...register("phone")}}
-                  errorMessage={errors.phone?.message}
-                />
-              </div>
-
-              <fieldset className="col-span-6 grid grid-cols-2 gap-0">
-                <legend className="block mb-2 text-sm font-medium text-slate-700">
-                  Card Details
-                </legend>
-
-                <div className="col-span-2">
-                  <label htmlFor="cardNumber" className="sr-only"> Card Number </label>
-                  <Input 
-                    id="cardNumber"
-                    placeholder="Card Number"
-                    autocomplete="cc-number"
-                    inputAttributes={{...register("cardNumber")}}
-                    errorMessage={errors.cardNumber?.message}
-                  />
-                </div>
-
-                <div className="col-span-1">
-                  <label htmlFor="CardExpiry" className="sr-only"> Card Expiry </label>
-                  <Input 
-                    id="cardExpiry"
-                    placeholder="Expiry Date"
-                    autocomplete="cc-exp"
-                    inputAttributes={{...register("cardExpiry")}}
-                    errorMessage={errors.cardExpiry?.message}
-                  />
-                </div>
-
-                <div className="col-span-1">
-                  <label htmlFor="CardCVC" className="sr-only"> Card CVC </label>
-                  <Input 
-                    id="cardCVC"
-                    placeholder="CVC"
-                    autocomplete="off"
-                    inputAttributes={{...register("cardCVC")}}
-                    errorMessage={errors.cardCVC?.message}
-                  />
-                </div>
-
-              </fieldset>
-
-              <fieldset className="col-span-6 grid grid-cols-1 gap-0">
-                <legend className="block mb-2 text-sm font-medium text-slate-700">
-                  Billing Address
-                </legend>
-
-                  <div>
-                    <label htmlFor="Country" className="sr-only">Country</label>
-
-                    <select
-                      id="Country"
-                      {...register("country")}
-                      autoComplete="country"
-                      className="relative w-full border-slate-200 rounded-t-xs focus:z-10 sm:text-sm"
-                    >
-                      <option></option>
-                      <option>England</option>
-                      <option>Wales</option>
-                      <option>Poland</option>
-                      <option>Scotland</option>
-                      <option>France</option>
-                      <option>Belgium</option>
-                      <option>Japan</option>
-                    </select>
-                  </div>
-
-                  <div>
-                    <label className="sr-only" htmlFor="PostalCode"> ZIP/Post Code </label>
-                    <Input 
-                      id="postalCode"
-                      placeholder="ZIP/Post Code"
-                      autocomplete="postal-code"
-                      inputAttributes={{...register("postalCode")}}
-                      errorMessage={errors.postalCode?.message}
-                    />
-
-                  </div>
-              </fieldset>
-
-              <div className="col-span-6">
-                <button
-                  className="block w-full mt-4 rounded-xs bg-red-400 font-bold p-3 text-sm text-white transition hover:shadow-lg"
-                >
-                  Pay Now
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
+    <form 
+      onSubmit={handleSubmit(onSubmit)} 
+      className="grid grid-cols-6 gap-4"
+    >
+      <div className="col-span-6">
+        <Input 
+          id="FirstName"
+          label="Name"
+          autocomplete="name"
+          inputAttributes={{...register("name")}}
+          errorMessage={errors.name?.message}
+        />
       </div>
-    </section>
+
+      <div className="col-span-6">
+        <Input 
+          id="Email"
+          label="E-mail"
+          autocomplete="email"
+          type="email"
+          inputAttributes={{...register("email")}}
+          errorMessage={errors.email?.message}
+        />
+      </div>
+
+      <div className="col-span-6">
+        <Input 
+          id="phone"
+          label="Phone"
+          autocomplete="tel"
+          type="tel"
+          inputAttributes={{...register("phone")}}
+          errorMessage={errors.phone?.message}
+        />
+      </div>
+
+      <fieldset className="col-span-6 grid grid-cols-2 gap-0">
+        <legend className="block mb-2 text-sm font-medium text-slate-700">
+          Card Details
+        </legend>
+
+        <div className="col-span-2">
+          <label htmlFor="cardNumber" className="sr-only"> Card Number </label>
+          <Input 
+            id="cardNumber"
+            placeholder="Card Number"
+            autocomplete="cc-number"
+            inputAttributes={{...register("cardNumber")}}
+            errorMessage={errors.cardNumber?.message}
+          />
+        </div>
+
+        <div className="col-span-1">
+          <label htmlFor="CardExpiry" className="sr-only"> Card Expiry </label>
+          <Input 
+            id="cardExpiry"
+            placeholder="Expiry Date"
+            autocomplete="cc-exp"
+            inputAttributes={{...register("cardExpiry")}}
+            errorMessage={errors.cardExpiry?.message}
+          />
+        </div>
+
+        <div className="col-span-1">
+          <label htmlFor="CardCVC" className="sr-only"> Card CVC </label>
+          <Input 
+            id="cardCVC"
+            placeholder="CVC"
+            autocomplete="off"
+            inputAttributes={{...register("cardCVC")}}
+            errorMessage={errors.cardCVC?.message}
+          />
+        </div>
+
+      </fieldset>
+
+      <fieldset className="col-span-6 grid grid-cols-1 gap-0">
+        <legend className="block mb-2 text-sm font-medium text-slate-700">
+          Billing Address
+        </legend>
+
+          <div>
+            <label htmlFor="Country" className="sr-only">Country</label>
+
+            <select
+              id="Country"
+              {...register("country")}
+              autoComplete="country"
+              className="relative w-full border-slate-200 rounded-t-xs focus:z-10 sm:text-sm"
+            >
+              <option></option>
+              <option>England</option>
+              <option>Wales</option>
+              <option>Poland</option>
+              <option>Scotland</option>
+              <option>France</option>
+              <option>Belgium</option>
+              <option>Japan</option>
+            </select>
+          </div>
+
+          <div>
+            <label className="sr-only" htmlFor="PostalCode"> ZIP/Post Code </label>
+            <Input 
+              id="postalCode"
+              placeholder="ZIP/Post Code"
+              autocomplete="postal-code"
+              inputAttributes={{...register("postalCode")}}
+              errorMessage={errors.postalCode?.message}
+            />
+
+          </div>
+      </fieldset>
+
+      <div className="col-span-6">
+        <button
+          className="block w-full mt-4 rounded-xs bg-red-400 font-bold p-3 text-sm text-white transition hover:shadow-lg"
+        >
+          Pay Now
+        </button>
+      </div>
+    </form>
   );
 }
--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -5,7 +5,19 @@ const CheckoutPage = () => {
   return (
     <Main>
       <h1 className="uppercase text-lg font-bold text-slate-600 py-12 px-8">Checkout</h1>
-      <CheckoutForm/>
+      <section>
+        <div className="grid grid-cols-1 mx-auto max-w-screen-2xl md:grid-cols-2 border md:divide-x">
+          <div className="py-16 px-8">
+            <h2 className="uppercase text-md font-bold text-slate-600 mb-12 ">Cart summary</h2>
+          </div>
+    
+          <div className="py-16 px-8">
+            <div className="max-w-lg px-8 mx-auto">      
+              <CheckoutForm/>
+            </div>
+          </div>
+        </div>
+      </section>
     </Main>
   );
 }
